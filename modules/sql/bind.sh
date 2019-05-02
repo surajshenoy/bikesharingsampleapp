648cbee155152f05b2d6ee506ca509b443123747
John Stallo <johnsta@microsoft.com>
2019-05-02 06:44:41 -0700

    Formatting

--- modules/sql/bind.sh
+++ modules/sql/bind.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-# validate parameters
+#############################################
+# Validate parameters
 if [ -z "$AZURE_SUBSCRIPTION" ]; then
   AZURE_SUBSCRIPTION=$($az account show --query id -o tsv)
 fi
@@ -11,7 +12,8 @@ if [ -z "$AZURE_KEYVAULT_NAME" ]; then
   exit 1
 fi
 
-# Azure CLI
+#############################################
+# Find Azure CLI
 az=$(which az 2>/dev/null || true)
 if [ -z "$az" ]; then
   az=az.cmd
@@ -20,8 +22,9 @@ fi
 # TODO: Check role that como.yaml has required (hard-coding to 'reader-writer' for now)
 USER_ROLE=reader-writer
 
+#############################################
 # Retrieve SQL secrets from Azure KeyVault:
-#   user-data-server, user-data-database, user-data-reader-writer-username, user-data-reader-writer-password
+#   server, database, username, password (secret keys are prefixed with $COMO_NAME)
 
 SERVER=$($az keyvault secret show \
   --subscription $AZURE_SUBSCRIPTION --vault-name $AZURE_KEYVAULT_NAME \
@@ -43,5 +46,6 @@ PASSWORD=$($az keyvault secret show \
   --name $COMO_NAME-$USER_ROLE-password \
   --query value -o tsv)
 
+#############################################
 # Return SQL configuration
 echo '{"server":"'"$SERVER"'", "database":"'"$DATABASE"'", "username":"'"$USERNAME"'", "password":"'"$PASSWORD"'"}'
